28c987bad2933fcd514958dbdf915f2a1e61c1b3
Samuel Hedges <schedges@vt.edu>
2025-05-04 17:35:43 -0400

    Update collisionParser.cpp

--- collisionParser/collisionParser.cpp
+++ collisionParser/collisionParser.cpp
@@ -38,9 +38,9 @@ int main(int argc, char* argv[]) {
 
   //Srim settings
   float startOffset_nm = 1.0;  //From SRIM input, 10 A = 1nm, we start partway in to allow backscattered particles to show
-  float maxEnergy_keV = 5000;  //We start our SRIM sim slightly above this, at least 100 keV to allow for "burn in"
+  float maxEnergy_keV = 2000;  //We start our SRIM sim slightly above this, at least 100 keV to allow for "burn in"
 
-  int maxEntriesPerBin = 5e4;  //Avoid filling up too much data at lower energys
+  int maxEntriesPerBin = 25e3;  //Avoid filling up too much data at lower energys
   float binSize_keV = 5;       //Bin size
   
   //
