7195123bfac4f7fa624e0afa07dcf3c57758e651
Samuel Hedges <schedges@vt.edu>
2025-07-25 19:39:05 -0400

    Update trimRunner.py
    Fixed bug in olivine definition, trying to gzip collison and delete temp srim folder but not working yet

--- trimRunner.py
+++ trimRunner.py
@@ -4,16 +4,28 @@ import pickle
 import uuid
 import shutil
 import gzip
+import tarfile
 
 #Incident ion
-ionName = "2H"
-energy = 10500 #keV
-nps = 300000
+ionName = "16O"
+energy = 5500 #keV
+nps = 250000
 
 #Mass evaluation from https://www-nds.iaea.org/amdc/ame2020/mass_1.mas20.txt
 #subtracting off # of protons * 0.000548579905 amu to remove electron contribution
 massDict = {
+  "58Fe": 57.91901049747,
+  "57Fe": 56.92112887247,
+  "56Fe": 55.92067245947,  #91.8%
+  "54Fe": 53.92534511147,
+  "30Si": 29.96609001833,
+  "29Si": 28.96881454567,
+  "28Si": 27.96924641575,   #92.2%
+  "26Mg": 25.97601001314,
+  "25Mg": 24.97925400714,
+  "24Mg": 23.97845873014,   #79%
   "19F": 18.993465942925,
+  "16O": 15.99052598002,    #99.8%
   "7Li": 7.014357694545,
   "6Li": 6.013477147705,
   "4He": 4.00150609432,
@@ -23,7 +35,17 @@ massDict = {
   "1H": 1.007276451993
 }
 zDict = {
+  "58Fe": 26,
+  "57Fe": 26,
+  "56Fe": 26,
+  "30Si": 14,
+  "29Si": 14,
+  "28Si": 14,
+  "26Mg": 12,
+  "25Mg": 12,
+  "24Mg": 12,
   "19F": 9,
+  "16O": 8,
   "7Li": 3,
   "6Li": 3,
   "4He": 2,
@@ -37,26 +59,42 @@ mass = massDict[ionName]
 runMode = 2
 
 #Target description
-target_name = "LiF"
-target_nElements = 2
+target_name = "Olivine"
+if target_name == "LiF":
+  target_nElements = 2
+elif target_name == "Olivine":
+  target_nElements = 4
 target_nLayers = 1
 target_start_offset = 0 #Angstroms, we offset the start point to save backscattered ions
 
 #Note if you want the same element in different layers, you can optionally include it twice to track
 #recoils separately and set separate displacement energies
-target_element_names = ["Li","F"]
-target_element_Zs = [3,9]
-target_element_masses = [6.941,18.998]
-target_element_TDEs = [25,25] #eV, displacement energy for each element.
-target_element_LBEs = [3., 3.] #eV, lattice binding energies
-target_element_SBEs = [1.67, 2] #eV, surface binding energies
-
-layer_names = ["LiF"]
+if target_name=="LiF":
+  target_element_names = ["Li","F"]
+  target_element_Zs = [3,9]
+  target_element_masses = [6.941,18.998] #Natural abundances, atomic masses
+  target_element_TDEs = [25,25] #eV, displacement energy for each element.
+  target_element_LBEs = [3., 3.] #eV, lattice binding energies
+  target_element_SBEs = [1.67, 2] #eV, surface binding energies
+  layer_names = ["LiF"]
+  layer_densities  = [2.635] # g/cm^3]
+  #stoichs correspond to target_elements
+  layer_stoichs = [[0.5,0.5]]
+elif target_name == "Olivine":
+  target_element_names = ["Fe","Mg","Si","O"]
+  target_element_Zs = [26,12,14,8]
+  target_element_masses = [55.845,24.305,28.085,15.999]
+  #From https://theses.hal.science/tel-01126887v1/file/2014PA112184.pdf
+  target_element_TDEs = [25,25,25,25] #eV, displacement energy for each element.
+  target_element_LBEs = [1.9,1.9,4,15.5] #eV, lattice binding energies
+  target_element_SBEs = [6.47,3.7,6.8,4.75] #eV, surface binding energies
+  layer_names = ["Olivine"]
+  #https://webmineral.com/data/Olivine.shtml
+  layer_densities  = [3.32] # g/cm^3]
+  #stoichs correspond to target_elements
+  layer_stoichs = [[0.4,1.6,1.0,4.0]]
 layer_depths = [20000000] #Angstroms. (2mm) - we want to be sure ions won't range out
 target_depth = sum(layer_depths)
-layer_densities  = [2.635] # g/cm^3]
-#stoichs correspond to target_elements
-layer_stoichs = [[0.5,0.5]]
 layer_phases = [1]
 
 def makeTrimInputString(energy):
@@ -160,39 +198,43 @@ if __name__ == "__main__":
   shutil.copytree("C:/Users/Sam/Desktop/SRIM_exe", temp_workdir)
   srimFolder = temp_workdir + "/"
 
-  #Make output folder, output file names
+  # Make output folder, output file names
   outputFolder = "C:/Users/Sam/Documents/code/trimRunner/data/txt/"
   if not os.path.exists(outputFolder):
     os.mkdir(outputFolder)
 
-  txt_name = "{0}_{1}.txt".format(target_name,ionName)
+  txt_name = "{0}_{1}.txt".format(target_name, ionName)
   txt_path = os.path.join(outputFolder, txt_name)
-  gz_path = txt_path + ".gz"
+
+  # Overwrite if exists
+  if os.path.exists(txt_path):
+    os.remove(txt_path)
+  tar_path =  "{0}_{1}.tar.gz".format(target_name, ionName)
+  if os.path.exists(tar_path):
+    os.remove(tar_path)
 
   # Create new SRIM input
   os.chdir(srimFolder)
   lines = makeTrimInputString(energy)
-  trimFile = open("{0}TRIM.in".format(srimFolder),"w")
-  for line in lines:
-    if not line.endswith("\n"):
-      line+="\n"
+  with open(f"{srimFolder}TRIM.in", "w") as trimFile:
+    for line in lines:
+      if not line.endswith("\n"):
+        line += "\n"
       trimFile.write(line)
-  trimFile.close()
+    trimFile.close()
 
-  #Run SRIM  
+  # Run SRIM  
   exit_code = os.system("TRIM.exe")
 
-  #Upon successful completion, mv collison.txt to file name, compress, remove temp folder
+  # Upon successful completion, mv collison.txt to file name, compress, remove temp folder
   if exit_code == 0:
     print("TRIM completed successfully.")
 
     # Move and compress output
     src = os.path.join(srimFolder, "SRIM Outputs", "COLLISON.txt")
-    os.rename(src, txt_path) 
-    with open(txt_path, 'rb') as f_in:
-      data = f_in.read()
-    with gzip.open(gz_path, 'wb') as f_out:
-      f_out.write(data)
-    os.remove(txt_path)
+    os.replace(src, txt_path)
 
-  shutil.rmtree(srimFolder)
+    # Create .tar.gz
+    with tarfile.open(tar_path, "w:gz") as tar:
+      tar.add(txt_path, arcname=os.path.basename(txt_path))
+      tar.close()
