8740f4337b213786a1a223da82a7be8811828d5f
Samuel Hedges <schedges@vt.edu>
2025-07-25 19:39:40 -0400

    Update root_to_h5.py
    Updated branch names

--- root_to_h5.py
+++ root_to_h5.py
@@ -13,43 +13,47 @@ with uproot.open(root_file) as f:
   tree = f[treeName]
   print("Reading branches...")
   nEntries = tree.num_entries
-  energies_eV = np.empty(nEntries, dtype=np.uint32)
+  ionEnergy_eV = np.empty(nEntries, dtype=np.uint32)
   xs_nm = [None] * nEntries
   ys_nm = [None] * nEntries
   zs_nm = [None] * nEntries
   nVacs = [None] * nEntries
+  displacedAtoms_Z = [None] * nEntries
+  recoilEnergies_eV = [None] * nEntries
 
   batch_size = 10000
   for start in tqdm.trange(0, nEntries, batch_size, desc="Reading in batches"):
       stop = min(start + batch_size, nEntries)
-      arr = tree.arrays(["energy_eV", "xs_nm", "ys_nm", "zs_nm", "nVacs"],
+      arr = tree.arrays(["ionEnergy_eV", "xs_nm", "ys_nm", "zs_nm", "nVacs","displacedAtoms_Z","recoilEnergies_eV"],
                         entry_start=start, entry_stop=stop,
                         library="np")
-      energies_eV[start:stop] = arr["energy_eV"]
+      ionEnergy_eV[start:stop] = arr["ionEnergy_eV"]
       xs_nm[start:stop] = arr["xs_nm"]
       ys_nm[start:stop] = arr["ys_nm"]
       zs_nm[start:stop] = arr["zs_nm"]
       nVacs[start:stop] = arr["nVacs"]
+      displacedAtoms_Z[start:stop] = arr["displacedAtoms_Z"]
+      recoilEnergies_eV[start:stop] = arr["recoilEnergies_eV"]
       
 print("Writing HDF5 file...")
 with h5py.File(output_file, "w") as h5f:
-  h5f.create_dataset("energies_eV", data=energies_eV)
+  h5f.create_dataset("ionEnergy_eV", data=ionEnergy_eV)
   # Define variable-length datatypes
   dt_float = h5py.vlen_dtype(np.float32)
-  dt_int = h5py.vlen_dtype(np.int32)
+  dt_uint8 = h5py.vlen_dtype(np.uint8)
+  dt_int32 = h5py.vlen_dtype(np.int32)
   # Create empty datasets
   dset_xs = h5f.create_dataset("xs_nm", shape=(len(xs_nm),), dtype=dt_float)
   dset_ys = h5f.create_dataset("ys_nm", shape=(len(ys_nm),), dtype=dt_float)
   dset_zs = h5f.create_dataset("zs_nm", shape=(len(zs_nm),), dtype=dt_float)
-  dset_nv = h5f.create_dataset("nVacs", shape=(len(nVacs),), dtype=dt_int)
+  dset_nv = h5f.create_dataset("nVacs", shape=(len(nVacs),), dtype=dt_uint8)
+  dset_displacedZ = h5f.create_dataset("displacedAtoms_Z", shape=(len(displacedAtoms_Z),), dtype=dt_uint8)
+  dset_recoilEs = h5f.create_dataset("recoilEnergies_eV", shape=(len(recoilEnergies_eV),), dtype=dt_int32)
   for i in tqdm.tqdm(range(len(xs_nm)),total=len(xs_nm)):
     dset_xs[i] = xs_nm[i]
     dset_ys[i] = ys_nm[i]
     dset_zs[i] = zs_nm[i]
     dset_nv[i] = nVacs[i]
+    dset_displacedZ[i] = displacedAtoms_Z[i]
+    dset_recoilEs[i] = recoilEnergies_eV[i]
 print(f"Saved HDF5 to {output_file}")
-
-
-
-
-
